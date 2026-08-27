f400e8a3a238ea608f441c260b4377bba146d4dd
Lyne <patriciamagne02@gmail.com>
2026-08-27 17:43:55 -0400

    annexe1

--- Annexe1/app/src/main/java/com/lyne/annexe1/AfficherActivity.kt
+++ Annexe1/app/src/main/java/com/lyne/annexe1/AfficherActivity.kt
@@ -1,12 +1,18 @@
 package com.lyne.annexe1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class AfficherActivity : AppCompatActivity() {
+    lateinit var liste: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,25 @@ class AfficherActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        liste = findViewById(R.id.listeMemo)
+
+        liste.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lireMemos())
+    }
+
+    fun lireMemos(): ArrayList<String>
+    {
+        val fis = openFileInput("fichier.txt")
+        val isr = InputStreamReader(fis)
+
+        val br = BufferedReader(isr)
+
+        val a = ArrayList<String>()
+
+        br.use{
+            br.forEachLine { ligne -> a.add(ligne) }
+
+            //a = br.readLines() as ArrayList<String> //as utilisé pour transtypé
+        }
+        return a
     }
 }
